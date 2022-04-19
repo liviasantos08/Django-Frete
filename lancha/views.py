a61ec525cda2d40cff68beac5210386888e1cb76
liviasantos08 <liviasantos08@hotmail.com>
2022-04-19 19:41:21 -0300

    Adição de forms
    Adição do formulário na blank page.

--- lancha/views.py
+++ lancha/views.py
@@ -2,14 +2,21 @@ from django.shortcuts import render
 from django.views import View
 
 
-class Index(View):
-    template = 'index.html'
+class Base(View):
+    template = 'base.html'
 
     def get(self, request):
         return render(request, self.template)
 
 class Login(View):
     template = 'login.html'
+
+    def get(self, request):
+        return render(request, self.template)
+
+
+class Cadastro(View):
+    template = 'index.html'
 
     def get(self, request):
         return render(request, self.template)
